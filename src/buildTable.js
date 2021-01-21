97279442dddebcdc8e08085f5fcc811b0ca68d2b
jdittrich <d_jan@ymail.com>
2021-01-21 17:44:42 +0100

    correct types for buildTable.js

--- src/buildTable.js
+++ src/buildTable.js
@@ -1,9 +1,10 @@
 /**
 * 
 * @param {object[]} table
-* @param {string} table[].title - title of the ebook
+* @param {string} table[].   - title of the ebook
 * @param {string} table[].date - when the note was made
 * @param {string} table[].page – page the note was made on
+* @param {string} table[].type – type of the note (e.g. selection, note, bookmark…)
 * @param {string} table[].content – content of the note. 
 */
  
